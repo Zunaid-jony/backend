ee942b5e5bf009d14dc53502540605e9c2bfc666
Zunaid-jony <jony35666666@gmail.com>
2022-06-30 02:31:59 +0600

    product

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 import express from "express";
-import data from './data.js';
+import data from "./data.js";
 
 const app = express();
 
-
-app.get('/api/products', (req,res) =>{
-    res.send(data.products)
-})
-
-const port = process.env.PORT || 5000;
-app.listen(port, () =>{
-    console.log(`server ai http://localhost:${port}`)
+app.get("/api/products", (req, res) => {
+  res.send(data.products);
+});
+// only product
+app.get("/api/products/slug/:slug", (req, res) => {
+  const product = data.products.find((x) => x.slug === req.params.slug);
+  if (product) {
+    res.set(product);
+  } else {
+    res.status(404).send({ message: "product Not Found" });
+  }
 });
 
-
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`server ai http://localhost:${port}`);
+});
