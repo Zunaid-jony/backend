ef92a5c2a5f044318f0b5df5cf71f8bbd4ba23f3
Zunaid-jony <jony35666666@gmail.com>
2022-06-30 23:36:37 +0600

    data update

--- server.js
+++ server.js
@@ -3,16 +3,16 @@ import data from "./data.js";
 
 const app = express();
 
-app.get("/api/products", (req, res) => {
+app.get('/api/products', (req, res) => {
   res.send(data.products);
 });
-// only product
-app.get("/api/products/slug/:slug", (req, res) => {
+
+app.get('/api/products/slug/:slug', (req, res) => {
   const product = data.products.find((x) => x.slug === req.params.slug);
   if (product) {
-    res.set(product);
+    res.send(product);
   } else {
-    res.status(404).send({ message: "product Not Found" });
+    res.status(404).send({ message: 'Product Not Found' });
   }
 });
 
