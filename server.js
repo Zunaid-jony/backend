3007fd2689813f892c12560a8ab6f16b9cd7deef
Zunaid-jony <jony35666666@gmail.com>
2022-07-04 23:58:31 +0600

    c

--- server.js
+++ server.js
@@ -6,7 +6,10 @@ import dotenv from 'dotenv'
 dotenv.config();
 
 mongoose.connect(process.env.MONGODB_URI).then(()=>{
+ 
   console.log('connected to bd')
+}).catch((err)=>{
+  console.log(err.message)
 })
 
 const app = express();
